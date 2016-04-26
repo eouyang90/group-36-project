1ef2383aa8fbd701625dd8df4ffca376c88eab5e
Jiayuan Chen <chenjiayuancz@gmail.com>
2016-04-26 14:53:59 -0700

    start CPR activity 1

--- mobile/src/main/java/group36/cpr/StartCPRActivity1.java
+++ mobile/src/main/java/group36/cpr/StartCPRActivity1.java
@@ -7,7 +7,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 
 /**
  * Created by austinhle on 4/15/16.
@@ -18,13 +18,38 @@ public class StartCPRActivity1 extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cpr_activity_1);
 
-        ImageButton startCPRButton = (ImageButton) findViewById(R.id.CPR1Background);
-        startCPRButton.setOnClickListener(new View.OnClickListener() {
+        //adult
+        ImageView adult_selection = (ImageView) findViewById(R.id.adult_select);
+        adult_selection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent sendIntent;
-                sendIntent = new Intent(getBaseContext(), StartCPRActivity2.class);
-                Log.d("MainActivity", "Starting up StartCPRActivity1");
+                Intent sendIntent = new Intent(getBaseContext(), StartCPRActivity2.class);
+                sendIntent.putExtra("selection", "adult");
+                Log.d("StartCPRActivity1", "Selected adult");
+                startActivity(sendIntent);
+            }
+        });
+
+        //child
+        ImageView child_selection = (ImageView) findViewById(R.id.child_select);
+        child_selection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent(getBaseContext(), StartCPRActivity2.class);
+                sendIntent.putExtra("selection", "child");
+                Log.d("StartCPRActivity1", "Selected child");
+                startActivity(sendIntent);
+            }
+        });
+
+        //infant
+        ImageView infant_selection = (ImageView) findViewById(R.id.infant_select);
+        infant_selection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendIntent = new Intent(getBaseContext(), StartCPRActivity2.class);
+                sendIntent.putExtra("selection", "infant");
+                Log.d("StartCPRActivity1", "Selected infant");
                 startActivity(sendIntent);
             }
         });
